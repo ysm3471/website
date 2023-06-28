71d4af6e3fecdf425982a8169fbcb1733dfbc1f6
ysm3471 <ysm3471@gmail.com>
2023-06-28 20:16:13 +0900

    1차

--- components/Layout/SmallHeader/SmallHeader.jsx
+++ components/Layout/SmallHeader/SmallHeader.jsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 export default function SmallHeader() {
   return (
     <div className={classes.SmallHeader}>
-      <h1><Link href="http://localhost:3000/">대문</Link></h1>
+      <h1><Link href="https://website-eight-cyan-86.vercel.app/">대문</Link></h1>
     </div>
   )
 }
